729655e8ab2bf6239fd82d0595a27e3e6ac312f3
moha <moha_ali@live.nl>
2023-09-18 11:19:32 +0200

    scaffolded the lingo nestjs api

---
+++ apps/lingo-api/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'lingo-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/lingo-api',
+};
