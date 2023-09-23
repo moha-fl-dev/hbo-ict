95b63dc66e93be65e82f223c6b205a68aea78ecd
moha <moha_ali@live.nl>
2023-09-23 17:30:57 +0200

    configured shadcn. finished theming and font

--- apps/lingo/app/page.tsx
+++ apps/lingo/app/page.tsx
@@ -1,7 +1,51 @@
+import {
+  Button,
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+  AlertDialogHeader,
+} from '@hbo-ict/ui';
+
 export default async function Index() {
   return (
     <div>
-      <h1 className="text-3xl text-red-600 font-bold">Index</h1>
+      <div className="flex flex-row justify-center mt-5">
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-row gap-2">
+            <Button variant={'default'}>Default</Button>
+            <Button variant={'secondary'}>Secondary</Button>
+            <Button variant={'destructive'}>Destructive</Button>
+            <Button variant={'ghost'}>Ghost</Button>
+            <Button variant={'link'}>Link</Button>
+            <Button variant={'outline'}>Outline</Button>
+          </div>
+          <div className="flex flex-row gap-2">
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button variant={'secondary'}>Open</Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    your account and remove your data from our servers.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogAction>Continue</AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ libs/shared/ui/src/index.ts
@@ -0,0 +1,13 @@
+export * from './lib/ui';
+export { Button, type buttonVariants } from './lib/components/button';
+export {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogAction,
+  AlertDialogCancel,
+} from './lib/components/alert-dialog';
