ef370e99d4f0902c6eceb12c99e31c527fb78a58
moha <moha_ali@live.nl>
2023-09-23 16:32:44 +0200

    added tailwind and scaffolded sign-in and sign-up pages

---
+++ apps/lingo/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+export default function SignIn() {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-600 font-bold">Sign In</h1>
+    </div>
+  );
+}
---
+++ apps/lingo/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUp() {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-600 font-bold">Sign Up</h1>
+    </div>
+  );
+}
