25f4d93f2c16cbaee9cb6067a266639f31823f46
moha <moha_ali@live.nl>
2023-11-06 21:56:36 +0100

    very simple 1/3 a/b testing implemented for comment section

--- libs/shared/ui/src/lib/blocks/comments-block.tsx
+++ libs/shared/ui/src/lib/blocks/comments-block.tsx
@@ -1,13 +1,11 @@
 import { useFindComment } from '@hbo-ict/hooks';
-import { CommentDefaultReturn, CommentType } from '@hbo-ict/lingo/types';
-import Link from 'next/link';
+import { CommentDefaultReturn } from '@hbo-ict/lingo/types';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import { formatDate } from '../utils';
 
 export function Comments() {
   const router = useRouter();
-
+  const randomValue = Math.random();
   const { comments, isError, isLoading } = useFindComment({
     where: {
       ticket: {
@@ -21,16 +19,32 @@ export function Comments() {
     },
   });
 
+  if (randomValue < 1 / 3) {
+    <div className="w-full flex flex-col gap-2">
+      {comments?.map((comment) => (
+        <CommentA key={comment.id} {...comment} />
+      ))}
+    </div>;
+  } else {
+    return (
+      <div className="w-full flex flex-col gap-2">
+        {comments?.map((comment) => (
+          <CommentB key={comment.id} {...comment} />
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div className="w-full flex flex-col gap-2">
       {comments?.map((comment) => (
-        <Comment key={comment.id} {...comment} />
+        <CommentA key={comment.id} {...comment} />
       ))}
     </div>
   );
 }
 
-function Comment(comment: CommentDefaultReturn) {
+function CommentA(comment: CommentDefaultReturn) {
   return (
     <div className="flex gap-2">
       <div className="flex flex-col gap-2">
@@ -45,6 +59,26 @@ function Comment(comment: CommentDefaultReturn) {
         <p className="leading-relaxed text-primary/90 text-sm">
           {comment.content}
         </p>
+      </div>
+    </div>
+  );
+}
+
+function CommentB(comment: CommentDefaultReturn) {
+  return (
+    <div className="bg-secondary/50 rounded-md p-2" key={comment.id}>
+      <div className="flex flex-col gap-2">
+        <div>
+          <span className="text-xs text-primary font-semibold">
+            {comment.author.name}
+          </span>
+        </div>
+        <div className="text-sm text-primary/90 ">
+          <p className="leading-relaxed">{comment.content}</p>
+        </div>
+        <div className="text-xs flex fle-row justify-end">
+          <span>{formatDate(comment.createdAt)}</span>
+        </div>
       </div>
     </div>
   );
