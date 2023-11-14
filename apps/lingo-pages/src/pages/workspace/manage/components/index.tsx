64743464681e39ba9832392d9abe8a993d3fa1b7
moha <moha_ali@live.nl>
2023-11-14 15:38:54 +0100

    redesigned components page. not sure if i love it but good practice

--- apps/lingo-pages/src/pages/workspace/manage/components/index.tsx
+++ apps/lingo-pages/src/pages/workspace/manage/components/index.tsx
@@ -1,19 +1,20 @@
 import { useComponent, useTeam } from '@hbo-ict/hooks';
 import {
+  Badge,
   Button,
   ComponentsLayout,
-  ScrollArea,
-  Separator,
   SummaryLink,
   WorkspaceRootLayout,
 } from '@hbo-ict/ui';
 import { TicketStatusEnum } from '@prisma/client/lingo';
 import {
+  ChevronRightIcon,
   Link1Icon,
   LinkNone1Icon,
   LockClosedIcon,
   LockOpen1Icon,
 } from '@radix-ui/react-icons';
+import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
@@ -30,116 +31,118 @@ export default function ManageComponents() {
 
   if (!componentId) {
     return (
-      <div className="col-span-4 bg-slate-50/50 flex flex-col items-center justify-center">
-        <span className="text-slate-200">Select a component</span>
+      <div className="flex flex-col items-center justify-center h-full">
+        <span className="text-slate-300">Select a component</span>
       </div>
     );
   }
 
   if (!component || !team) {
     return (
-      <div className="col-span-4 bg-slate-50/50 flex flex-col items-center justify-center">
+      <div className="bg-slate-50/50 flex flex-col items-center justify-center h-full">
         <span className="text-slate-200">Component not found</span>
       </div>
     );
   }
 
   return (
-    <div className="col-span-4 bg-slate-50/50 flex flex-col justify-around items-center gap-2 p-4">
-      <div className="flex flex-col items-center">
-        <Button
-          asChild
-          variant={'link'}
-          className="font-bold text-3xl text-slate-400"
-        >
-          <Link
-            href={`/workspace/manage/departments/?department=${encodeURIComponent(
-              team.Department.id as string,
-            )}`}
-          >
-            {team.Department.name}
-          </Link>
-        </Button>
-        <Separator orientation="vertical" />
-        <Button
-          asChild
-          variant={'link'}
-          className="font-bold text-3xl text-slate-400"
-        >
-          <Link href={`/workspace/manage/teams/?team=${team.id}`}>
-            {team.name}
-          </Link>
-        </Button>
-        <Separator orientation="vertical" />
-
-        <h1 className="font-bold text-3xl text-slate-400">{component.name}</h1>
-        <Separator orientation="vertical" />
+    <div className="h-[80vh]">
+      <div className="border-b">
+        <div className="flex flex-row justify-between items-center p-2">
+          <div className="flex flex-row items-center p-2">
+            <Button variant={'ghost'} asChild>
+              <Link
+                href={`/workspace/manage/departments?department=${team.Department.id}`}
+                className="flex flex-row items-center text-gray-600 hover:text-gray-800 transition-colors"
+              >
+                <span>{team.Department.name}</span>
+                <ChevronRightIcon />
+              </Link>
+            </Button>
+            <Button variant={'ghost'} asChild>
+              <Link
+                href={`/workspace/manage/teams?team=${team.id}`}
+                className="flex flex-row gap-1 items-center text-gray-600 hover:text-gray-800 transition-colors"
+              >
+                <span>{team.name}</span>
+                <ChevronRightIcon />
+              </Link>
+            </Button>
+            <Button variant={'ghost'}>{component.name}</Button>
+          </div>
+          <div className="px-2">
+            <Badge>Operational</Badge>
+          </div>
+        </div>
       </div>
-      <div className="grid grid-cols-2 gap-2">
-        <SummaryLink
-          max={100}
-          min={10}
-          icon={<LockOpen1Icon />}
-          label={'Total open'}
-          href={`/workspace/tickets?component=${encodeURIComponent(
-            component.id,
-          )}&ticket_status=${
-            TicketStatusEnum.OPEN
-          }&department=${encodeURIComponent(
-            team.Department.id,
-          )}&team=${encodeURIComponent(team.id)}`}
-        />
+      <div className="flex flex-col items-center justify-center h-full">
+        <div className="grid grid-cols-2 gap-2">
+          <SummaryLink
+            max={100}
+            min={10}
+            icon={<LockOpen1Icon />}
+            label={'Total open'}
+            href={`/workspace/tickets?component=${encodeURIComponent(
+              component.id,
+            )}&ticket_status=${
+              TicketStatusEnum.OPEN
+            }&department=${encodeURIComponent(
+              team.Department.id,
+            )}&team=${encodeURIComponent(team.id)}`}
+          />
 
-        <SummaryLink
-          max={100}
-          min={10}
-          icon={<LockClosedIcon />}
-          label={'Total closed'}
-          href={`/workspace/tickets?component=${encodeURIComponent(
-            component.id,
-          )}&ticket_status=${
-            TicketStatusEnum.CLOSED
-          }&department=${encodeURIComponent(
-            team.Department.id,
-          )}&team=${encodeURIComponent(team.id)}`}
-        />
-        <SummaryLink
-          max={100}
-          min={10}
-          icon={<Link1Icon />}
-          label={'Total assigned'}
-          href={`/workspace/tickets?component=${encodeURIComponent(
-            component.id,
-          )}&ticket_status=${
-            TicketStatusEnum.ACTIVE
-          }&department=${encodeURIComponent(
-            team.Department.id,
-          )}&team=${encodeURIComponent(team.id)}`}
-        />
-        <SummaryLink
-          max={100}
-          min={10}
-          icon={<LinkNone1Icon />}
-          label={'Total unassigned'}
-          href={`/workspace/tickets?component=${encodeURIComponent(
-            component.id,
-          )}&ticket_status=${
-            TicketStatusEnum.HOLD
-          }&department=${encodeURIComponent(
-            team.Department.id,
-          )}&team=${encodeURIComponent(team.id)}`}
-        />
+          <SummaryLink
+            max={100}
+            min={10}
+            icon={<LockClosedIcon />}
+            label={'Total closed'}
+            href={`/workspace/tickets?component=${encodeURIComponent(
+              component.id,
+            )}&ticket_status=${
+              TicketStatusEnum.CLOSED
+            }&department=${encodeURIComponent(
+              team.Department.id,
+            )}&team=${encodeURIComponent(team.id)}`}
+          />
+          <SummaryLink
+            max={100}
+            min={10}
+            icon={<Link1Icon />}
+            label={'Total assigned'}
+            href={`/workspace/tickets?component=${encodeURIComponent(
+              component.id,
+            )}&ticket_status=${
+              TicketStatusEnum.ACTIVE
+            }&department=${encodeURIComponent(
+              team.Department.id,
+            )}&team=${encodeURIComponent(team.id)}`}
+          />
+          <SummaryLink
+            max={100}
+            min={10}
+            icon={<LinkNone1Icon />}
+            label={'Total unassigned'}
+            href={`/workspace/tickets?component=${encodeURIComponent(
+              component.id,
+            )}&ticket_status=${
+              TicketStatusEnum.HOLD
+            }&department=${encodeURIComponent(
+              team.Department.id,
+            )}&team=${encodeURIComponent(team.id)}`}
+          />
+        </div>
       </div>
     </div>
   );
 }
 
-ManageComponents.getLayout = function getLayout(_page: JSX.Element) {
+ManageComponents.getLayout = function getLayout(page: JSX.Element) {
   return (
     <WorkspaceRootLayout>
-      <ScrollArea className="max-h-[90vh] h-[90vh] min-h[90vh]">
-        <ComponentsLayout />
-      </ScrollArea>
+      <Head>
+        <title>Manage components</title>
+      </Head>
+      <ComponentsLayout>{page}</ComponentsLayout>
     </WorkspaceRootLayout>
   );
 };
--- libs/shared/ui/src/lib/layouts/components.layout.tsx
+++ libs/shared/ui/src/lib/layouts/components.layout.tsx
@@ -1,14 +1,13 @@
 import { useComponents, useTeams } from '@hbo-ict/hooks';
-import type { Component, CreateComponentDto } from '@hbo-ict/lingo/types';
-import { TicketStatusEnum, createComponentSchema } from '@hbo-ict/lingo/types';
+import type { CreateComponentDto } from '@hbo-ict/lingo/types';
+import { createComponentSchema } from '@hbo-ict/lingo/types';
 import { Api } from '@hbo-ict/query-fns';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { LockOpen1Icon, PlusIcon } from '@radix-ui/react-icons';
-import { ScrollArea } from '@radix-ui/react-scroll-area';
+import { PlusIcon } from '@radix-ui/react-icons';
 
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import Link from 'next/link';
-import router from 'next/router';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Button } from '../components/button';
@@ -22,6 +21,7 @@ import {
   FormMessage,
 } from '../components/form';
 import { Input } from '../components/input';
+import { ScrollArea } from '../components/scroll-area';
 import {
   Select,
   SelectContent,
@@ -38,10 +38,10 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '../components/sheet';
-import { SummaryLink } from '../components/summaryLink';
 import { toast } from '../hooks/use-toast';
 
-export function ComponentsLayout() {
+export function ComponentsLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
   const [openSheet, setOpenSheet] = useState<boolean>(false);
   const queryClient = useQueryClient();
 
@@ -77,165 +77,147 @@ export function ComponentsLayout() {
   }
 
   return (
-    <div className="">
-      <div className="flex flex-row justify-end">
-        <Sheet open={openSheet}>
-          <SheetTrigger asChild>
-            <Button
-              variant={'workspace'}
-              size={'sm'}
-              className="h-8"
-              onClick={() => setOpenSheet(true)}
-            >
-              <div className="flex flex-row gap-2 items-center">
-                <PlusIcon fontSize={10} />
-                <span>New</span>
-              </div>
-            </Button>
-          </SheetTrigger>
-          <SheetContent side={'right'} className="p-0 md:max-w-xl sm:max-w-xl">
-            <SheetHeader>
-              <SheetTitle className="p-4">Add new Component</SheetTitle>
-              <Separator />
-            </SheetHeader>
-            <div className="flex flex-col justify-between ">
-              <Form {...form}>
-                <form
-                  onSubmit={form.handleSubmit(onSubmit)}
-                  className="flex flex-col gap-4 p-4"
-                >
-                  <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Component name</FormLabel>
-                        <FormControl>
-                          <Input placeholder="CMS" {...field} type="text" />
-                        </FormControl>
-                        <FormDescription>
-                          This is the public display name for the component.
-                        </FormDescription>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="team.id"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Team</FormLabel>
-                        <Select
-                          onValueChange={field.onChange}
-                          defaultValue={field.value}
-                        >
-                          <FormControl>
-                            <SelectTrigger>
-                              <SelectValue placeholder="Select the team that owns this component" />
-                            </SelectTrigger>
-                          </FormControl>
-                          <SelectContent>
-                            <ScrollArea className="max-h-[400px]">
-                              {teams?.map((team) => (
-                                <SelectItem
-                                  className={`hover:bg-slate-50 transition-colors ${
-                                    field.value === team.id && 'bg-slate-100'
-                                  }`}
-                                  key={team.id}
-                                  value={team.id}
-                                >
-                                  {team.name}
-                                </SelectItem>
-                              ))}
-                            </ScrollArea>
-                          </SelectContent>
-                        </Select>
-                        <FormDescription>
-                          You can manage teams in{' '}
-                          <Link href="/workspace/manage/teams">
-                            team setting
-                          </Link>
-                          .
-                        </FormDescription>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <Button variant={'workspace'} type="submit">
-                    Create component
-                  </Button>
-                </form>
-              </Form>
-              <div>
-                <Separator />
-                <div className="flex flex-row gap-2 content-end items-end justify-end p-4">
-                  <Button
-                    variant={'outline'}
-                    onClick={() => setOpenSheet(false)}
+    <div className="container shadow-lg p-4 h-[90vh]">
+      <div className="border h-full">
+        <div className="grid grid-cols-5 h-full">
+          <div className="row-span-full lg:col-span-1 col-span-2 border-r">
+            <div className="border-b">
+              <div className="p-4">
+                <Sheet open={openSheet}>
+                  <SheetTrigger asChild>
+                    <Button
+                      variant={'workspace'}
+                      className="w-full"
+                      onClick={() => setOpenSheet(true)}
+                    >
+                      <div className="flex flex-row gap-2 items-center">
+                        <PlusIcon fontSize={10} />
+                        <span>New</span>
+                      </div>
+                    </Button>
+                  </SheetTrigger>
+                  <SheetContent
+                    side={'right'}
+                    className="p-0 md:max-w-xl sm:max-w-xl"
                   >
-                    Cancel
-                  </Button>
-                </div>
+                    <SheetHeader>
+                      <SheetTitle className="p-4">Add new Component</SheetTitle>
+                      <Separator />
+                    </SheetHeader>
+                    <div className="flex flex-col justify-between ">
+                      <Form {...form}>
+                        <form
+                          onSubmit={form.handleSubmit(onSubmit)}
+                          className="flex flex-col gap-4 p-4"
+                        >
+                          <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                              <FormItem>
+                                <FormLabel>Component name</FormLabel>
+                                <FormControl>
+                                  <Input
+                                    placeholder="CMS"
+                                    {...field}
+                                    type="text"
+                                  />
+                                </FormControl>
+                                <FormDescription>
+                                  This is the public display name for the
+                                  component.
+                                </FormDescription>
+                                <FormMessage />
+                              </FormItem>
+                            )}
+                          />
+                          <FormField
+                            control={form.control}
+                            name="team.id"
+                            render={({ field }) => (
+                              <FormItem>
+                                <FormLabel>Team</FormLabel>
+                                <Select
+                                  onValueChange={field.onChange}
+                                  defaultValue={field.value}
+                                >
+                                  <FormControl>
+                                    <SelectTrigger>
+                                      <SelectValue placeholder="Select the team that owns this component" />
+                                    </SelectTrigger>
+                                  </FormControl>
+                                  <SelectContent>
+                                    <ScrollArea className="max-h-[400px]">
+                                      {teams?.map((team) => (
+                                        <SelectItem
+                                          className={`hover:bg-slate-50 transition-colors ${
+                                            field.value === team.id &&
+                                            'bg-slate-100'
+                                          }`}
+                                          key={team.id}
+                                          value={team.id}
+                                        >
+                                          {team.name}
+                                        </SelectItem>
+                                      ))}
+                                    </ScrollArea>
+                                  </SelectContent>
+                                </Select>
+                                <FormDescription>
+                                  You can manage teams in{' '}
+                                  <Link href="/workspace/manage/teams">
+                                    team setting
+                                  </Link>
+                                  .
+                                </FormDescription>
+                                <FormMessage />
+                              </FormItem>
+                            )}
+                          />
+                          <Button variant={'workspace'} type="submit">
+                            Create component
+                          </Button>
+                        </form>
+                      </Form>
+                      <div>
+                        <Separator />
+                        <div className="flex flex-row gap-2 content-end items-end justify-end p-4">
+                          <Button
+                            variant={'outline'}
+                            onClick={() => setOpenSheet(false)}
+                          >
+                            Cancel
+                          </Button>
+                        </div>
+                      </div>
+                    </div>
+                  </SheetContent>
+                  <SheetOverlay
+                    onClick={() => setOpenSheet(false)}
+                    className="backdrop-blur-0 "
+                  />
+                </Sheet>
               </div>
             </div>
-          </SheetContent>
-          <SheetOverlay
-            onClick={() => setOpenSheet(false)}
-            className="backdrop-blur-0 "
-          />
-        </Sheet>
-      </div>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-2 mt-4">
-        {components?.map((component, i) => (
-          <ComponentCard key={i} component={component} />
-        ))}
-      </div>
-    </div>
-  );
-}
-
-function ComponentCard({ component }: { component: Component }) {
-  return (
-    <div className="border rounded p-4 shadow-sm">
-      <h3 className="text-lg font-semibold">{component.name}</h3>
-      <div className="grid grid-cols-2 gap-2 mt-4">
-        <SummaryLink
-          max={100}
-          min={10}
-          icon={<LockOpen1Icon />}
-          label={'Total open'}
-          href={`/workspace/tickets?component=${encodeURIComponent(
-            component.id,
-          )}&ticket_status=${TicketStatusEnum.OPEN}`}
-        />
-        <SummaryLink
-          max={100}
-          min={10}
-          icon={<LockOpen1Icon />}
-          label={'Total active'}
-          href={`/workspace/tickets?component=${encodeURIComponent(
-            component.id,
-          )}&ticket_status=${TicketStatusEnum.ACTIVE}`}
-        />
-        <SummaryLink
-          max={100}
-          min={10}
-          icon={<LockOpen1Icon />}
-          label={'Total on hold'}
-          href={`/workspace/tickets?component=${encodeURIComponent(
-            component.id,
-          )}&ticket_status=${TicketStatusEnum.HOLD}`}
-        />
-        <SummaryLink
-          max={100}
-          min={10}
-          icon={<LockOpen1Icon />}
-          label={'Total closed'}
-          href={`/workspace/tickets?component=${encodeURIComponent(
-            component.id,
-          )}&ticket_status=${TicketStatusEnum.CLOSED}`}
-        />
+            <ScrollArea className="h-[75vh] max-h-[75vh]">
+              <div className="flex flex-col w-full ">
+                {components?.map((component, i) => (
+                  <Link
+                    key={i}
+                    className={`${
+                      router.query.component === component.id &&
+                      'bg-workspace-secondary'
+                    } p-4 transition-colors hover:text-workspace-foreground border-b`}
+                    href={`/workspace/manage/components/?component=${component.id}`}
+                  >
+                    <span className="font-medium">{component.name}</span>
+                  </Link>
+                ))}
+              </div>
+            </ScrollArea>
+          </div>
+          <div className="row-span-full col-span-6">{children}</div>
+        </div>
       </div>
     </div>
   );
