e99b3ad6d1a921ad86e2f2986324e6ed94501efc
moha <moha_ali@live.nl>
2023-12-03 11:40:03 +0100

    removed data-access god module

--- libs/lingo/data-access/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './lib/data-access.module';
-export * from './lib/department.service';
-export * from './lib/team.service';
-export * from './lib/component.service';
-export * from './lib/employee.service';
-export * from './lib/ticketNumber.service';
-export * from './lib/ticket.service';
