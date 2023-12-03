b8178807dc0cc80070d162e1de34986bd84ddef5
moha <moha_ali@live.nl>
2023-12-03 11:45:21 +0100

    exported tokens gaurd from the supabase auth module

--- apps/lingo-api/src/auth/auth.module.ts
+++ apps/lingo-api/src/auth/auth.module.ts
@@ -4,12 +4,12 @@ import {
   GlobalGuard,
   SupabaseAuthModule,
   SupabaseStrategy,
+  TokensGuard,
 } from '@hbo-ict/supabase-auth';
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { TokensGuard } from 'libs/lingo/auth/src/lib/guard/tokens.guard';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
--- libs/lingo/auth/src/index.ts
+++ libs/lingo/auth/src/index.ts
@@ -1,5 +1,6 @@
+export * from './lib/guard/global.guard';
+export * from './lib/guard/tokens.guard';
 export * from './lib/supabase-auth.module';
 export * from './lib/supabase/guard/supabase.guard';
 export * from './lib/supabase/strategy/supabase.strategy';
 export * from './lib/supabase/supabase.service';
-export * from './lib/guard/global.guard';
