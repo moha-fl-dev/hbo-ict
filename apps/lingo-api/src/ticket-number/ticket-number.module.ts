196b8e225faa4af59004799c826243e8e7bbf759
moha <moha_ali@live.nl>
2023-12-03 11:28:46 +0100

    removed non-module ticket and ticket-number. modules already exist

--- apps/lingo-api/src/ticket-number/ticket-number.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TicketNumberController } from './ticket-number.controller';
-import { TicketNumberService } from '@hbo-ict/data-access';
-
-@Module({
-  controllers: [TicketNumberController],
-  providers: [TicketNumberService],
-})
-export class TicketNumberModule {}
